77a8dc52c2762794de2d271f8b32b7aba63c2ba8
Joseph Haaga <haaga.joe@gmail.com>
2020-04-28 22:58:34 -0400

    attaching events!

--- modules/mimicslider/includes/frontend.js.php
+++ modules/mimicslider/includes/frontend.js.php
@@ -1,5 +1,36 @@
 jQuery(function($) {
-	$('.fl-node-<?php echo $id; ?> .fl-mimic-slider').slick();
-	var target_slider_name = "<?php echo $settings->name_of_target_slider; ?>";
-	console.log("name_of_target_slider=" + target_slider_name);
+	
+	const thisSliderName = "<?php echo $settings->slider_name; ?>";
+	if (thisSliderName.length < 1) {
+		console.warn("Mimic Slider at .fl-node-<?php echo $id; ?> has no slider_name!" + 
+		"Please visit the module settings in Page Builder and add a name.");
+	}
+	
+	let controlsAnotherSlider = false;
+	const targetSliderName = "<?php echo $settings->name_of_target_slider; ?>";
+	if(targetSliderName.length < 1) {
+		console.log("Mimic Slider at .fl-node-<?php echo $id; ?> does not control another Mimic Slider.");
+	} else {
+		controlsAnotherSlider = true;
+	}
+
+	// TODO could this be a const?
+	let thisSlider = $('.fl-node-<?php echo $id; ?> .fl-mimic-slider').slick({
+		// config go skrra
+	});
+
+	if (controlsAnotherSlider) {
+		// TODO could this go after the window.mimicSliders registration?
+		const targetSlider = window.mimicSliders[targetSliderName]
+		thisSlider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+			targetSlider.slickGoTo(nextSlide);
+		});
+	}
+
+	// Register this slider so other Mimic Sliders can see it.
+	if (window.mimicSliders === undefined) {
+		window.mimicSliders = {};
+	}
+	window.mimicSliders[thisSliderName] = thisSlider;
+
 });
