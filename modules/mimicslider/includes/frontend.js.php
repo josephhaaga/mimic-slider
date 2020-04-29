fe66470247944e02b040ed5e34d7875fadc8bf8e
Joseph Haaga <haaga.joe@gmail.com>
2020-04-28 22:39:12 -0400

    echoing target slider name so we can logic check

--- modules/mimicslider/includes/frontend.js.php
+++ modules/mimicslider/includes/frontend.js.php
@@ -1,3 +1,5 @@
 jQuery(function($) {
 	$('.fl-node-<?php echo $id; ?> .fl-mimic-slider').slick();
+	var target_slider_name = "<?php echo $settings->name_of_target_slider; ?>";
+	console.log("name_of_target_slider=" + target_slider_name);
 });
